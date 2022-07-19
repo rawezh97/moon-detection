95cb121eecf76e210ad42442c1f1918b9cb925cb
Rawe Zh <92225352+rawezh97@users.noreply.github.com>
2022-07-19 11:15:59 +0300

    Update moon1.py

--- moon1.py
+++ moon1.py
@@ -4,7 +4,6 @@ import numpy
 video = cv2.VideoCapture("moon.mp4")
 object_detection = cv2.createBackgroundSubtractorMOG2()
 while True:
-
     boolean , frame = video.read()
     frame = cv2.resize(frame,(700,700))
     copyframe = frame.copy()
@@ -12,14 +11,10 @@ while True:
     imgray = cv2.cvtColor(frame , cv2.COLOR_BGR2GRAY)
     imblur = cv2.GaussianBlur(imgray,(5,5),1)
     imcanny = cv2.Canny(imblur,150,0)
-
-
+    
     countours , hieararchy = cv2.findContours(imcanny , cv2.RETR_EXTERNAL , cv2.CHAIN_APPROX_NONE)
     cv2.drawContours(copyframe , countours , -1 , (0,255,0),2)
-
-
-
-
+  
     cv2.imshow("copyframe",copyframe)
     cv2.imshow("gray",imgray)
     #cv2.imshow("blur",imblur)
@@ -28,6 +23,5 @@ while True:
     key = cv2.waitKey(1)
     if key == 27:
         break
-
 video.release()
 cv2.distroyAllWindows()
